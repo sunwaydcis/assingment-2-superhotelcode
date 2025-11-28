2c235fd207d4f5855f16481bd33387b10926fd76
MPXW2 <mpxw2@MPXW2s-MacBook-Pro.local>
2025-11-28 17:01:04 +0800

    #8 Optimize code

---
+++ src/main/scala-3/NumberUtil.scala
@@ -0,0 +1,8 @@
+object NumberUtil:
+  def formatFloatValue(value: Float, delimiter: Int = 3): Float =
+    var d = 1
+    for (i <- 1 to delimiter) {
+      d *= 10
+    }
+    // e.g. delimiter = 2 -> 123.23, 3 -> 123.235
+    Math.round(value * d) / d.toFloat
